f858beaee6a3b0c572462517667aeb523a3d4587
Amaru333 <amrutesharun0599@gmail.com>
2023-07-27 17:09:37 +0530

    footer changes

--- src/components/LandingPage/Footer.js
+++ src/components/LandingPage/Footer.js
@@ -4,7 +4,7 @@ function Footer() {
   return (
     <div className="bg-gradient-to-t from-[#a33b6f36] to-black">
       <div className="max-w-screen-2xl mx-auto px-6 grid grid-cols-12 text-[#727272] pt-40 pb-16 2xl:pb-40 gap-y-8 text-sm sm:text-base">
-        <div className="col-span-12 sm:col-span-4 lg:col-span-6 flex flex-row justify-between items-center sm:flex-col">
+        <div className="col-span-12 sm:col-span-4 lg:col-span-6 flex flex-row justify-between items-center sm:items-start sm:flex-col">
           <div>
             <img src="/logo.svg" className="mb-4" />
             <p>On-Chain Search</p>

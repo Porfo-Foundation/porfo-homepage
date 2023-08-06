1c3c09f22b69e350aa95785cc7210dc1b4fcb4af
Amaru333 <amrutesharun0599@gmail.com>
2023-08-06 19:09:59 +0530

    kickoff page

--- src/components/LandingPage/HeroSection.js
+++ src/components/LandingPage/HeroSection.js
@@ -34,7 +34,7 @@ function HeroSection() {
         <div className="flex items-center w-full justify-center mt-6 2xl:mt-10">
           <div className="w-40 h-10 relative flex items-center justify-center group">
             <div className="w-10 h-10 bg-gradient-to-tr from-[#A33B6F] to-[#FF4D00] absolute right-1 top-0 rounded-full group-hover:w-40 transition-all duration-300"></div>
-            <button onClick={() => setIsGetStartedModalOpen(true)} className="flex items-center text-sm absolute top-0 left-0 w-full pl-5">
+            <button onClick={() => window.open("https://porfo-waitlist.kickoffpages.com/", "_blank")} className="flex items-center text-sm absolute top-0 left-0 w-full pl-5">
               <p className="group-hover:text-black transition-all duration-300">Get Started</p>
               <div className="h-10 w-10 flex items-center justify-center rounded-full ml-4">
                 <img src="/landing-page/right-arrow.svg" />
@@ -44,14 +44,19 @@ function HeroSection() {
         </div>
       </div>
       <div className="flex flex-col items-center">
-      <p className="from-[#F679B7] to-[#A33B6F] inline-block text-transparent bg-clip-text bg-gradient-radial mt-6">In Collaboration With</p>
-      <br />
+        <p className="from-[#F679B7] to-[#A33B6F] inline-block text-transparent bg-clip-text bg-gradient-radial mt-6">In Collaboration With</p>
+        <br />
         <div className="flex gap-x-10">
           {partners?.map((partner, index) => (
             <img src={`/landing-page/${partner.image}.png`} className="w-12 h-12 2xl:w-14 2xl:h-14" key={index} />
           ))}
         </div>
-        <p className="from-[#F679B7] to-[#A33B6F] inline-block text-transparent bg-clip-text bg-gradient-radial mt-6">Incubated @ <a href="https://thewasserstoff.com" style={{textDecoration : "none"}} target="_blank">The Wasserstoff</a></p>
+        <p className="from-[#F679B7] to-[#A33B6F] inline-block text-transparent bg-clip-text bg-gradient-radial mt-6">
+          Incubated @{" "}
+          <a href="https://thewasserstoff.com" style={{ textDecoration: "none" }} target="_blank">
+            The Wasserstoff
+          </a>
+        </p>
       </div>
     </div>
   );

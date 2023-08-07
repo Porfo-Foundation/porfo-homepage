439dcd26d9bf37ed85bbbef9580c3429a75dac65
Wasserstoff Innovation & Learning Labs <72060276+wasserstoff-india@users.noreply.github.com>
2023-08-07 15:35:52 +0000

    logo change

--- src/components/LandingPage/Navbar.js
+++ src/components/LandingPage/Navbar.js
@@ -50,7 +50,7 @@ function Navbar() {
         <GetStartedModal />
       </UIModal>
       <div className="justify-between items-center max-w-[1740px] mx-auto px-12 py-4 hidden md:flex">
-        <img src="/logo.png" className="w-30" />
+        <img src="/logo.png" className="min-w-30 max-w-[100px]" />
         <div className="flex gap-x-6">
           {navbarLinks?.map((link, index) => (
             <Link

6d0c52e050261aba1798ba2a7643dd8e4da86dcb
ayush tripathi <ayush_wstf@ayushs-MacBook-Air.local>
2024-01-25 14:56:12 +0530

    changes

--- app/page.js
+++ app/page.js
@@ -132,6 +132,7 @@ export default function Home() {
               </motion.div>
                 
             </div>)
+            
           }
           
           <div className='overflow-x-hidden'>
